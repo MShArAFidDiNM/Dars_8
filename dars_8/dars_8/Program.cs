e83e13e23a690ff79de27e846e536f1842c5e633
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-04-05 11:45:52 +0500

    Max Funksiya

--- dars_8/dars_8/Program.cs
+++ dars_8/dars_8/Program.cs
@@ -10,6 +10,39 @@ namespace dars_8
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Max : {INPUT(3)}");
+            Main(args);
         }
+        public static int INPUT(int a)
+        {
+        number1:
+            Console.Write("Enter Number 1 :");
+            string input = Console.ReadLine();
+            bool check = int.TryParse(input, out int inputnumber);
+            if (!check)
+            {
+                Console.WriteLine($"Error : {input}");
+                goto number1;
+            }
+            number2:
+            Console.Write("Enter Number 2 :");
+            string input2 = Console.ReadLine();
+            bool check2 = int.TryParse(input2, out int inputnumber2);
+            if (!check2)
+            {
+                Console.WriteLine($"Error : {input2}");
+                goto number2;
+            }
+            int b;
+            b = Max(inputnumber, inputnumber2);
+            return b;
+        }
+        
+        public static int Max(int input1,int input2)
+        {
+            return (input1 >  input2) ? input1 : input2;
+        }
+        
+        
     }
 }
